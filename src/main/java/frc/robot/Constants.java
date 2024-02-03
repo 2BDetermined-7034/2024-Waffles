b3f9fcff5f5186262972a9c0dc9a9ac0bf66ae39
Team7034 <33676445+Team7034@users.noreply.github.com>
2024-02-03 12:54:34 -0800

    Modified ClimbSubsystem to use PIDControllers because why not.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,9 +26,14 @@ public final class Constants
 
   public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
 
-  public static final double MAX_CLIMB_VELOCITY = 0.5;
-  public static final double CLIMB_MOTOR_TARGET_POSITION_REVOLUTIONS = 2.0;
-  public static final double CLIMB_MOTOR_START_POSITION_REVOLUTIONS = 0.0; //I don't think we need this btw - Jack M >:[
+  //ClimbSubsystem Constants
+  public static final class Climb {
+    public static final int cwMotorID = 255;
+    public static final int ccwMotorID = 256;
+    public static final double MAX_CLIMB_VELOCITY = 0.5;
+    public static final double CLIMB_MOTOR_TARGET_POSITION_REVOLUTIONS = 2.0;
+    public static final double CLIMB_MOTOR_START_POSITION_REVOLUTIONS = 0.0; //I don't think we need this btw
+  }
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
--- src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
@@ -3,37 +3,63 @@ package frc.robot.subsystems.climb;
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+import static frc.robot.Constants.Climb.*;
+
+/*
+*	Climbing Subsystem :
+* 	Motors		|	Function
+* 	-----------------------------
+* 	cwMotor 	|	Clockwise motor for one of the coils
+* 	ccwMotor	|	Counter-Clockwise motor for one of the coils
+*
+* 	NOTE:
+* 	The actual direction of the motor doesn't matter, as long as they are spinning in opposite directions.
+*/
 public class ClimbSubsystem extends SubsystemBase {
-	CANSparkMax cwMotor, ccwMotor;
+	protected CANSparkMax cwMotor, ccwMotor;
+	protected PIDController cwController, ccwController;
+	protected RelativeEncoder cwEncoder, ccwEncoder;
+	public double targetPosition;
 
 	public ClimbSubsystem() {
-		cwMotor = new CANSparkMax(255, CANSparkLowLevel.MotorType.kBrushless);
-		ccwMotor = new CANSparkMax(69, CANSparkLowLevel.MotorType.kBrushless);
+		cwMotor = new CANSparkMax(cwMotorID, CANSparkLowLevel.MotorType.kBrushless);
+		ccwMotor = new CANSparkMax(ccwMotorID, CANSparkLowLevel.MotorType.kBrushless);
 
-		boolean swapInverted = false;
-		ccwMotor.setInverted(swapInverted);
-		cwMotor.setInverted(!swapInverted);
+		final boolean swapInverted = true; //Modify if motors are running in the wrong direction
+		cwMotor.setInverted(swapInverted);
+		ccwMotor.setInverted(!swapInverted);
+
+		cwEncoder = cwMotor.getEncoder();
+		ccwEncoder = ccwMotor.getEncoder();
+
+		cwController = new PIDController(0.5, 0.0, 0.3);
+		ccwController = new PIDController(0.5, 0.0, 0.3);
+
+		targetPosition = CLIMB_MOTOR_START_POSITION_REVOLUTIONS;
 	}
 
+	//Useless
 	@Override
 	public void periodic() {
-
+		if (!cwController.atSetpoint())
+			cwEncoder.setPosition(cwController.calculate(cwEncoder.getPosition(), targetPosition));
+		if (!ccwController.atSetpoint())
+			ccwEncoder.setPosition(ccwController.calculate(ccwEncoder.getPosition(), targetPosition));
 	}
 
-	public void setTargetPosition(double position) {
-		cwMotor.getEncoder().setPosition(position);
-		ccwMotor.getEncoder().setPosition(position);
+	//Extend the climbing arm
+	public void extend() {
+		targetPosition = CLIMB_MOTOR_TARGET_POSITION_REVOLUTIONS;
 	}
 
-	public void erect() {
-		setTargetPosition(Constants.CLIMB_MOTOR_TARGET_POSITION_REVOLUTIONS);
-	}
-
+	//Retract the climbing arm
 	public void retract() {
-		setTargetPosition(Constants.CLIMB_MOTOR_START_POSITION_REVOLUTIONS); //It's zero btw - Jack M >:[
+		targetPosition = CLIMB_MOTOR_START_POSITION_REVOLUTIONS;
 	}
 }
 
