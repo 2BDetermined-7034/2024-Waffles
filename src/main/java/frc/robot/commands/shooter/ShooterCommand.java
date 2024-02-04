b984aacf594eb5946510053c19f12f2c293bcff5
Team7034 <wlhsfrc@gmail.com>
2024-02-03 18:51:13 -0800

    Added ShooterVision.java to a new branch "waffles-shooter"

--- src/main/java/frc/robot/commands/shooter/ShooterCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShooterCommand.java
@@ -1,26 +1,25 @@
 package frc.robot.commands.shooter;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 public class ShooterCommand extends Command {
-	private ShooterSubsystem shooter;
-
+	private ShooterSubsystem shooterSubsystem;
 	public ShooterCommand(ShooterSubsystem shooter) {
-		this.shooter = shooter;
+		this.shooterSubsystem = shooter;
 		addRequirements(shooter);
+	}
+	/**Gets the equation of the hypotenuse of a triangle for the purpose of aiming based on trigonometry.
+	 *<p>
+	 *Equation = arctan((80.57-h)/(27+d))
+	 *
+	 *
+	 * */
+	public double getEquationValue(double distance) {
+		return Math.atan(57.205/(27+distance));
 	}
 
 	@Override
 	public void execute() {
-		shooter.setVelocityTalon(0.3);
-		shooter.setAngleTalonVelocity(0.3);
-	}
-
-	@Override
-	public void end(boolean interrupted) {
-		shooter.setVelocityTalon(0);
-		shooter.setAngleTalonVelocity(0);
-		shooter.setIndexerNeo550Speed(0);
+		new ShooterSubsystem().setAngleTalonPosition(getEquationValue(0)); //TODO: find the distance and replace the parameter with it.
 	}
 }
