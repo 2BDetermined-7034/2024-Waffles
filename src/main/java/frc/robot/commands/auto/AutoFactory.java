9f7025531db789b70d07cdd05222cad6058d508d
npspy <username.lilrice@gmail.com>
2024-02-16 00:26:29 -0800

    Added on the fly path

--- src/main/java/frc/robot/commands/auto/AutoFactory.java
+++ src/main/java/frc/robot/commands/auto/AutoFactory.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.auto;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.path.PathPoint;
@@ -156,6 +157,22 @@ public class AutoFactory {
             throw new RuntimeException(ex);
         }
         return new WaitCommand(0);
+    }
+
+    public static Command flyPathFindWithPoses() {
+        List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(
+                new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
+                new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
+                new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90))
+        );
+
+        PathPlannerPath path = new PathPlannerPath(
+                bezierPoints,
+                new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI), // The constraints for this path. If using a differential drivetrain, the angular constraints have no effect.
+                new GoalEndState(0.0, Rotation2d.fromDegrees(-90)) // Goal end state. You can set a holonomic rotation here. If using a differential drivetrain, the rotation will have no effect.
+        );
+        path.preventFlipping = true;
+        return AutoBuilder.pathfindThenFollowPath(path, new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI));
     }
 
 
